64f2b0ba12a79266195321a5dfceffc3470a8e80
blurk <55154720+blurk@users.noreply.github.com>
2021-08-15 21:03:07 +0700

    feat: only 1 active sidebar item at 1 time

--- src/script/sidebar.js
+++ src/script/sidebar.js
@@ -6,6 +6,11 @@ window.addEventListener("DOMContentLoaded", function () {
 
   sidebarItems.forEach((item) => {
     item.addEventListener("click", function handleSidebarItemClick() {
+      sidebarItems.forEach((item) => {
+        if (item.classList.contains("sidebar__inner__item--active")) {
+          item.classList.remove("sidebar__inner__item--active");
+        }
+      });
       this.classList.toggle("sidebar__inner__item--active");
     });
   });
