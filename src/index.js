9728d3004ed21221e4980b8a41f43ab0936b4a94
Dat Dinh <>
2021-09-04 16:07:31 +0700

    add modal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import "@fortawesome/fontawesome-free/js/fontawesome";
-import "@fortawesome/fontawesome-free/js/solid";
-import "@fortawesome/fontawesome-free/js/regular";
-import "@fortawesome/fontawesome-free/js/brands";
-
 import "./index.scss";
 import "./script/sidebar";
 import "./script/modal";
